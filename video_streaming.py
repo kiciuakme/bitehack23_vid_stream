55bcd1fb2637bfe075d6957e23a8cb1489d05470
kiciuakme <49717885+kiciuakme@users.noreply.github.com>
2023-03-14 16:01:05 +0100

    Add files via upload

---
+++ video_streaming.py
@@ -0,0 +1,111 @@
+#Stream is available on adress below: 
+
+#http://<your_raspperry_ip>:8000
+
+import io
+import picamera2
+import logging
+import socketserver
+from threading import Condition
+from http import server
+
+PAGE="""\
+<html>
+<head>
+<title>Raspberry Pi - Surveillance Camera</title>
+</head>
+<body>
+<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
+</body>
+</html>
+"""
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(301)
+            self.send_header('Location', '/index.html')
+            self.end_headers()
+        elif self.path == '/index.html':
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+if __name__ == "__main__":
+    picam = picamera2.Picamera2()
+    try:
+        config = picam.create_video_configuration(main={
+            "size": (640, 480)
+        })
+        picam.configure(config)
+
+        encoder = picamera2.encoders.H264Encoder(10000000)
+        # picam.start_recording(encoder, 'test.h264')
+
+        output = StreamingOutput()
+        picam.start_recording(encoder, output, 'test.h264') #'mjpeg')
+
+    # with picam.Picamera2(resolution='640x480', framerate=24) as camera:
+        
+    
+        address = ('', 8000)
+        server = StreamingServer(address, StreamingHandler)
+        server.serve_forever()
+    except BaseException as e:
+        picam.close()
+        raise e
+    finally:
+        picam.stop_recording()
+
+# scp ./video_streaming.py pi@172.16.25.128:/home/pi/wizja/ && ssh pi@172.16.25.128 python3 /home/pi/wizja/video_streaming.py
